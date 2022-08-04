c1792176d07800b6629076630a179ec2de82c553
alexpitronot <alexpitronot@gmail.com>
2022-08-04 18:28:33 +0300

    L13-CLASS

--- מצגות/L12-sql.sql
+++ מצגות/L12-sql.sql
@@ -18,3 +18,14 @@ from cities
 select city,countries.country
 from cities
 inner join countries on cities.country = countries.ID
+
+select Products.Name,Products.Price,Products.Price*1.17 as Kolel_maam
+from Products
+
+select DISTINCT Category
+from Products
+
+
+select Products.Name,Products.Price
+from Products
+where price < 50
