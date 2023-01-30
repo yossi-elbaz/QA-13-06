54c426d805cc4cb17848190c23fc56cd69859a0e
alexpitronot <alexpitronot@gmail.com>
2023-01-30 18:02:35 +0200

    L50-class

---
+++ L50-1.py
@@ -0,0 +1,50 @@
+# import random
+print("Hello")
+print("Hi QA class!")
+# עזרה
+# help()
+
+# סוגי משתנה
+# num=2
+# print(type(num))
+
+# בוליאני
+# answer=True
+# print(type(answer))
+# print(2>1)
+
+# מספרים
+# a=22.5
+# b=30
+# c=a+b
+
+# print("a =",a)
+# print("b =",b)
+# print("c =",c)
+
+# print(type(c))
+
+# מס' אקראי
+
+# print("מס אקראי מ-1 עד 100: " + str(random.randrange(1,100)))
+# print("Random number from 1 to 100 is : " + str(random.randrange(1,100)))
+
+#תנאים
+# x = 21
+# if x == 21:
+#     print("Yes, x is 21")
+
+# if x == 21:
+# print("Yes, x is 21")
+
+# x = 20
+# if x != 21:
+#     print("Yes, x is not 21")
+
+# x = True
+# if x is True:
+#     print("Yes, x is True")
+
+# x = True
+# if x:
+#     print("Yes, x is True")
